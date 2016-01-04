331006554f13f18c1b90295812594c628f38b88a
Kitson Kelly <me@kitsonkelly.com>
2016-01-04 09:58:55 +0000

    Type dojo/NodeList-fx

---
+++ dojo/1.10/NodeList-fx.d.ts
@@ -0,0 +1,56 @@
+/// <reference path="dojo" />
+/// <reference path="_base" />
+
+declare namespace dojo {
+
+	interface NodeListAnimationArgs extends _base.AnimationArguments {
+		auto?: boolean;
+	}
+
+	interface NodeList<T extends Node> {
+		_anim(obj: any, method: string, args?: NodeListAnimationArgs): _base.Animation | this;
+
+		/**
+		 * wipe in all elements of this NodeList via `dojo/fx.wipeIn()`
+		 */
+		wipeIn(args: NodeListAnimationArgs): _base.Animation | this;
+
+		/**
+		 * wipe out all elements of this NodeList via `dojo/fx.wipeOut()`
+		 */
+		wipeOut(args: NodeListAnimationArgs): _base.Animation | this;
+
+		/**
+		 * slide all elements of the node list to the specified place via `dojo/fx.slideTo()`
+		 */
+		slideTo(args: NodeListAnimationArgs): _base.Animation | this;
+
+		/**
+		 * fade in all elements of this NodeList via `dojo.fadeIn`
+		 */
+		fadeIn(args: NodeListAnimationArgs): _base.Animation | this;
+
+		/**
+		 * fade out all elements of this NodeList via `dojo.fadeOut`
+		 */
+		fadeOut(args: NodeListAnimationArgs): _base.Animation | this;
+
+		/**
+		 * Animate all elements of this NodeList across the properties specified.
+		 * syntax identical to `dojo.animateProperty`
+		 */
+		animateProperty(args: NodeListAnimationArgs): _base.Animation | this;
+
+		/**
+		 * Animate one or more CSS properties for all nodes in this list.
+		 * The returned animation object will already be playing when it
+		 * is returned. See the docs for `dojo.anim` for full details.
+		 */
+		anim(properties: _base.AnimationArgumentsProperties, duration?: number, easing?: (n?: number) => number, onEnd?: Function, delay?: number): _base.Animation;
+	}
+}
+
+declare module 'dojo/NodeList-fx' {
+	const NodeList: dojo.NodeListConstructor;
+	export = NodeList;
+}
