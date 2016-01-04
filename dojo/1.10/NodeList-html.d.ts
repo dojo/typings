210a63c6a2d15bb1fd25543856de087c08c17b0b
Kitson Kelly <me@kitsonkelly.com>
2016-01-04 10:17:20 +0000

    Fix indentation in NodeList-html.d.ts

--- dojo/1.10/NodeList-html.d.ts
+++ dojo/1.10/NodeList-html.d.ts
@@ -1,15 +1,15 @@
 /// <reference path="dojo" />
 
 declare namespace dojo {
-    interface NodeList<T extends Node> {
-        /**
-         * see `dojo/html.set()`. Set the content of all elements of this NodeList
-         */
-        html(cont?: ContentSetterContent, params?: ContentSetterParams): this;
-    }
+	interface NodeList<T extends Node> {
+		/**
+		 * see `dojo/html.set()`. Set the content of all elements of this NodeList
+		 */
+		html(cont?: ContentSetterContent, params?: ContentSetterParams): this;
+	}
 }
 
 declare module 'dojo/NodeList-html' {
-    const NodeList: dojo.NodeListConstructor;
-    export = NodeList;
+	const NodeList: dojo.NodeListConstructor;
+	export = NodeList;
 }
