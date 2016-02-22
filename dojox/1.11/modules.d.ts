f6a0804af920fdb3f567e7088f68d817ac7a91da
Bill Schindler <bill@bitranch.com>
2016-02-22 09:34:15 +0000

    Type dojox/widget/DialogSimple, dojox/widget/Dialog, and dojox/widget/Toaster
    Awesomeness from @bitranch
    
    Closes #1
    Fixes #2

--- dojox/1.11/modules.d.ts
+++ dojox/1.11/modules.d.ts
@@ -1,1 +1,16 @@
 /// <reference path="index.d.ts" />
+
+declare module 'dojox/widget/DialogSimple' {
+	let DialogSimple: dojox.widget.DialogSimple;
+	export = DialogSimple;
+}
+
+declare module 'dojox/widget/Dialog' {
+	let Dialog: dojox.widget.Dialog;
+	export = Dialog;
+}
+
+declare module 'dojox/widget/Toaster' {
+	let Toaster: dojox.widget.ToasterConstructor;
+	export = Toaster;
+}
