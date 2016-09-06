f18fd7ae31ef5fbd1f21f2849e921cd2f9e49275
Matt Gadd <mgadd@sitepen.com>
2016-09-06 10:38:45 +0100

    remove superfluous remapped typings

--- custom/dojo2-extras/immutable.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * These provide re-exports of various modules that align to the typical
- * configuration of a Dojo 2 application
- */
-
-declare module 'immutable/immutable' {
-	export * from 'immutable';
-}
