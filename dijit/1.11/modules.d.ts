aaaf7ee6d28765fbed71e0ef2519708beaca7443
dylans <dylan@dojotoolkit.org>
2016-12-25 00:36:04 -0700

    Include remaining changes from #12

--- dijit/1.11/modules.d.ts
+++ dijit/1.11/modules.d.ts
@@ -103,6 +103,12 @@ declare module 'dijit/TitlePane' {
 	export = TitlePane;
 }
 
+declare module 'dijit/ToolbarSeparator' {
+	type ToolbarSeparate = dijit.ToolbarSeparator;
+        const ToolbarSeparator: dijit.ToolbarSeparator;
+        export = ToolbarSeparator;
+}
+
 declare module 'dijit/Tooltip' {
 	type Tooltip = dijit.Tooltip;
 	const Tooltip: dijit.TooltipConstructor;
@@ -228,6 +234,12 @@ declare module 'dijit/form/RangeBoundTextBox' {
 	export = RangeBoundTextBox;
 }
 
+declare module 'dijit/form/Select' {
+	type Select = dijit.form.Select;
+        const Select: dijit.form.Select;
+        export = Select;
+}
+
 declare module 'dijit/form/SimpleTextarea' {
 	type SimpleTextarea = dijit.form.SimpleTextarea;
 	const SimpleTextarea: dijit.form.SimpleTextareaConstructor;
@@ -335,3 +347,4 @@ declare module 'dijit/layout/TabController' {
 	const TabController: dijit.layout.TabControllerConstructor;
 	export = TabController;
 }
+
